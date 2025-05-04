286833a214895d6087b51c945c1486b772fe047d
Hon7374 <nguyentruonghung27032005@gmail.com>
2025-05-04 22:00:49 +0700

    lan 3

--- scripts/indexScripts.js
+++ scripts/indexScripts.js
@@ -1,13 +1,15 @@
-
 function setupSlider(id) {
     const slider = document.getElementById(id);
     const slides = slider.children;
     let currentIndex = 0;
+
+    // Đặt chiều rộng cho .slider theo số ảnh
+    slider.style.width = `${slides.length * 100}%`;
+
     setInterval(() => {
         currentIndex = (currentIndex + 1) % slides.length;
-        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+        slider.style.transform = `translateX(-${currentIndex * (100 / slides.length)}%)`;
     }, 5000);
 }
 
 setupSlider('slider');
-setupSlider('slider2');
